01be179520f616d68c825c5d66d25c2bf0fa90ce
sriprasanthcs <sriprasanthcs95@gmail.com>
2020-10-29 13:42:44 +0530

    https://github.com/sriprasanthcs/test.git

--- basic_table.php
+++ basic_table.php
@@ -100,7 +100,7 @@
                     $user_vals[] = $rows;
                   
                   }
-                  foreach ($user_vals as $user_val){//check
+                  foreach ($user_vals as $user_val){//chec
                    ?>
                   <tr>
                     <td><?php echo $user_val['Name']; ?></td>
