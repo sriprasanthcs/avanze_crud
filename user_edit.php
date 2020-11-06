2b67ebdf4d44dab3bf10d06e52e0abb2e54e1c61
sriprasanthcs <sriprasanthcs95@gmail.com>
2020-11-06 15:46:12 +0530

    ok

--- user_edit.php
+++ user_edit.php
@@ -11,7 +11,7 @@
 
   <title>Form Page</title>
 
-  <!-- Bootstrap CSS checks-->
+  <!-- Bootstrap CSS check-->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <!-- bootstrap theme -->
   <link href="css/bootstrap-theme.css" rel="stylesheet">
